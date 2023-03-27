097a7aa82838c07e998a615cc0ae218ff69f6e08
vishal <vishalshinde00126@gmail.com>
2023-03-27 17:41:32 +0530

    fix: add operator.js file

--- main.js
+++ main.js
@@ -34,8 +34,29 @@
 // console.log(isNaN(myNo)); // false it check is not a number
 // console.log(isNaN(name)); // true it check it is not number return true because it is a not a number.
 
-// NAN number challenges.
-console.log(NaN === NaN); // false
-console.log(Number.NaN === NaN); // false
-console.log(isNaN(NaN));  // true
-console.log(isNaN (Number.NaN)) // true
+// // NAN number challenges.
+// console.log(NaN === NaN); // false
+// console.log(Number.NaN === NaN); // false
+// console.log(isNaN(NaN)); // true
+// console.log(isNaN(Number.NaN)); // true
+// console.log(Number.NaN(Nan)); // true
+
+// data type challenges
+// 1.
+console.log(10 + "22");
+// 2.
+console.log(9 - "5"); // also its return 4 so it also but in javascript because it not possible to subtract string into num
+// 3
+console.log("javaScript" + "programming"); // javascript programming
+// 4
+console.log("" + ""); // return empty
+// 5
+console.log("" + 0); // return 0;
+// 6
+console.log("vishal" - "shinde"); // returns not a number
+// 7
+console.log(true + true); // return true
+// 8
+console.log(true + false); // true
+// 9
+console.log(false - true); // -1
---
+++ operator.js
@@ -0,0 +1,73 @@
+// //  what is expression
+// console.log(2 + 20);
+
+// // Assignment operator
+// var x = 4;
+// var y = 4;
+// console.log("give value are equal or not" + x == y);
+// // it return false because here we have to used + operator instead of that you have to used string literal
+// console.log(`given value are equal or not :  ${x == y}`);
+
+// Arithmetic Operator :
+// 1. addition (+)
+// 2. Subtraction (-)
+// 3. Multiplication (*)
+// 4. Division ( /) --> return dividend.
+// 5. Module(Reminder) (%) --> return reminder
+
+// Increment and Decrement Operator :
+// 2 type of increment and Decrement Operator 1. PostFix (x++) or (x--)  2. PreFix (++x) or (--x);
+
+//  post incremented :
+// // 1
+// var num = 30;
+// var num1 = num++;
+// console.log(num); // it return 31
+// console.log(num1); // it return 30;
+
+// // 2.
+// var no = 50;
+// var no1 = no++ + 5;
+// console.log(no1); // 55
+// console.log(no); // 51
+
+// preIncrement :
+// // 1
+// var num = 30;
+// var num1 = ++num;
+// console.log(num); // 31
+// console.log(num1); // 31
+
+// // 2.
+// var no = 50;
+// var no1 = ++no + 5;
+// console.log(no1); // 56
+// console.log(no); // 51
+
+// // Decrement
+// // postfix - Decrement :
+// // 1
+// var no = 50;
+// var no1 = no-- + 5;
+// console.log(no1); //55
+// console.log(no); // 49
+
+// // 2
+// var num = 30;
+// var num1 = num--;
+// console.log(num); // 29
+// console.log(num1); // 30
+
+// Decrement
+// prefix - Decrement :
+// 1
+var no = 50;
+var no1 = --no + 5;
+console.log(no1); //54
+console.log(no); // 49
+
+// 2
+var num = 30;
+var num1 = --num;
+console.log(num); // 29
+console.log(num1); // 29
