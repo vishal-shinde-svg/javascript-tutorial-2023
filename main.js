4c443662de948193586d3a27cecefa2a97d48486
vishal <vishalshinde00126@gmail.com>
2023-03-27 15:02:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// // perform operation on console..
+
+// // 1.
+// alert("Hello");
+
+// // 2.
+// console.log("Hello..");
+
+// //3. what is differences between null and undefined.
+
+// // you defined variable but not assigned a value called as undefined
+// var myName;
+// console.log("myName", myName);
+// console.log("undefined type  " + typeof myName);
+
+// // you defined variable and assigned a value as Null.
+// var myName = null;
+// console.log("myName", myName);
+// console.log("null type " + typeof myName);
+
+//4. what is Nan
+// Not-a-Number.
+var myNo = 1234567890;
+var myAdd = "pune";
+console.log("subtraction is" + myNo - myAdd);
+
+// 2.
+var myName = 'vishal';
+var
