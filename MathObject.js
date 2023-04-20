4821609183a44074366f91e90fc3bbaa7bb23014
vishal <vishalshinde00126@gmail.com>
2023-04-20 22:34:03 +0530

    fix: add math, time, and string function

---
+++ MathObject.js
@@ -0,0 +1,83 @@
+// Math object in javascript used to perform mathematical task on number
+
+// ex.
+// 1. Math.PI --> return default pi value
+// console.log(Math.PI);
+
+// // Math.round();--> return the value of x rounded to its nearest Integer.
+// let num = 10.500
+// console.log(Math.round(num));
+// let num1 = 10.233
+// console.log(Math.round(num1));
+
+// // Math.pow() --> this method return value of x of the power y
+
+// console.log(Math.pow(2, 3));
+// // same as
+// console.log(2 ** 3);
+
+
+// Math.sqrt() --> it return the square root of the given value value
+// console.log(Math.sqrt(81));
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(65));
+
+
+// Math.abs() --> return the absolute value of the (x) --> or given value
+
+// console.log(Math.abs(-55));
+// console.log(Math.abs(-55.5));
+// console.log(Math.abs(-955));
+// console.log(Math.abs(10));
+
+// if you perform subtraction and its return is - result using positive you have to used abs method to return positive result
+// console.log(Math.abs(4 - 6));
+
+
+// Math.ceil() - return the value of x rounded up to its nearest integer
+
+// console.log(Math.ceil(4.52));
+// console.log(Math.ceil(99.1));
+// console.log(Math.ceil(88.9));
+// console.log(Math.ceil(-99.8));
+
+
+
+// Math.floor() - return the value of x rounded down to its nearest integer
+
+// console.log(Math.floor(4.52));
+// console.log(Math.floor(99.1));
+// console.log(Math.floor(88.9));
+// console.log(Math.floor(-20.3));
+
+
+// Math.min() --> This method can be used to find lowest value in a list of arguments
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+
+
+// Math.max() --> This method can be used to find Highest value in a list of arguments
+// console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+
+
+// math.random() :
+// math.random () return the random number between 0 (inclusive) and 1 as (exclusive);
+// console.log(Math.random()); // it return random number between 0 to 9
+// console.log(Math.floor(Math.random() * 10)); // return the rounded number.between 0 to 9
+
+
+
+// Math.trunc() --> the trunc method return the integer part of number
+// it means if the number is positive or negative no matter it return with positive or negative sign.
+console.log(Math.trunc(10.9));
+console.log(Math.trunc(-99.8));
+
+
+
+
+
+
+
+
+
--- String.js
+++ String.js
@@ -39,13 +39,201 @@
 // 5* lastindexOf() -->
 // lastIndexOf() return the  index within the calling String object of the last occurrences of searchElement ,
 //  if not found it return -1;
-const bioData = 'hi i am vishal shinde at post junner';
-let lastIndex = bioData.lastIndexOf(('at'));
-console.log('lastIndex', lastIndex);
+// const bioData = 'hi i am vishal shinde at post junner';
+// let lastIndex = bioData.lastIndexOf(('at'));
+// console.log('lastIndex', lastIndex);
 
-// if not match return -1
-let lastIndex1 = bioData.lastIndexOf(('atm'));
-console.log('lastIndex', lastIndex1);
+// // if not match return -1
+// let lastIndex1 = bioData.lastIndexOf(('atm'));
+// console.log('lastIndex', lastIndex1);
+
+
+
+// // 6* The search method searches a string for specified value and return the position of the match
+// const bioData = 'hi i am vishal shinde at post junner';
+// const data = bioData.search('am');
+// console.log('data', data)
+
+// const notFound = bioData.search('ki');
+// console.log('notfound', notFound);
+
+
+
+// // 7 * slice () method
+// //  extract the part of the string and return extracted part as new string
+// // it having 2 argument start and end.
+// const bioData = 'hi i am vishal shinde at post junnar';
+// let newString = bioData.slice(2, 8); // you do not need to give end position it continue with whole string
+// // it return  extraction part as new string.
+// console.log('newString = ', newString); // it return  i am  because it does not contain end position in result
+// // also splice method does not change the original array
+// console.log('biodata', bioData);
+
+// Ex in slice method you have to pass -ve number
+
+// const bioData = 'hi i am vishal shinde at post junnar';
+// let data = bioData.slice(6, -2);
+// // if you pass end argument as negative it go throw the end of the string and subtract  the given end element of the string.
+// console.log('data =', data);
+
+
+// Q display only 280 characters of a string like the one used in twitter.
+
+// let myTweet = "multiple string.....('give long length string.')   "
+// console.log("myTweet", myTweet)
+// let ActualLength = myTweet.slice(0, 280);  // if you include 0 it count accurate 280
+// console.log('actualLength', ActualLength);
+
+
+
+// 8*  subString() --> used to extract the part of string and return extracted part as new string
+// subString does not accept negative value. if you give negative value it then the character counted from  0th position
+
+// let str = "Apple, Banana, Kiwi, Mango "
+// let res = str.substring(0, 4);
+// console.log(res);  // It return appl because it exclude the last index element
+
+
+// // 2
+// let str = "Apple, Banana, Kiwi, Mango "
+// // here you have to give negative index value so it count the 0th position.
+// let res = str.substring(7, -2);
+// console.log(res);
+
+
+//  9* subStr() :extract the part of string for given location and 2nd argument return the length or the
+// extracted part.
+// let str = "apple, mango, banana, kiwi";
+// let newStr = str.substr(7, 6); // it return the starting position 7 and the length of argument 6
+// // (i.e it return only 6 character)
+// console.log('newStr: ', newStr);
+
+
+// if you have to add 2nd argument as negative in subStr() method it does not return anything.
+// let str = "apple, mango, banana, kiwi";
+// let newStr = str.substr(7, -4); // if you give 2nd argument as -ve index value it does not return anything
+// console.log('newStr: ', newStr);
+
+// if you want to extract data form back side so you have to used subStr() method as single -ve value index
+// let str = "apple, mango, banana, kiwi";
+// let newStr = str.substr(-4);  // if you give the -ve value as single argument it return the data from back side.
+// console.log('newStr: ', newStr);
+
+
+
+//  10* replace method
+// replace specified value with another value in a string
+// replace having 2 arguments first one is searchbar, and 2nd is replaceFor
+// replace method work as casesensitive and it return only first match
+
+// let FullName = "i am vishal shinde";
+// let changeName = FullName.replace('vishal', 'vishu');
+// console.log('changeName', changeName);
+
+
+//  11* Extracting string characters
+
+// There are three method to extracting characters...
+// 1. charAt( position )
+// 2. charCodeAt( position )
+// 3. property access [].
+
+
+// // charAt( position )
+// // charAt method return the character at specified position in a string
+// let str = "apple, mango, banana, kiwi"
+// let newPosition = str.charAt(7);
+// console.log('newPosition : ', newPosition);
+// console.log('str : ', str)
+
+
+// // charCodeAt( position )
+// // charCodeAt() method return the uniCode of the  character at specified index position in a string
+// let str = "apple, mango, banana, kiwi"
+// let newPosition = str.charCodeAt(7);
+// console.log('newPosition : ', newPosition);
+// console.log('str : ', str)
+
+
+// property access [] ;
+// es5 (2000) allow property access on string.
+// let str = 'Hello word';
+// console.log(str[0]); // Here it is a array to access particular element
+// in in es5 it allows to string for property access [ ] ;
+
+
+// Other UseFull method
+
+let str = 'Hello word';
+
+// 1. toUpperCase():
+// const str1 = str.toUpperCase();
+// console.log('str1', str1);
+
+
+
+
+// 2. toLowerCase() :
+// const str1 = str.toLowerCase();
+// console.log('str1', str1);
+
+// 3. concat method
+// concat method used to join two or more string
+
+// let firstName = 'vishal'
+// let lastName = 'shinde'
+// let fullName = firstName.concat(lastName);
+// console.log('fullName : ', fullName);
+
+
+// 3. trim () method
+// trim() method used to remove white spaces form both side.
+// let string = '        Hello word     ';
+// console.log('string ', string);
+// console.log(string.length)
+// console.log('newTrimString : ', string.trim());
+// console.log(string.trim().length);
+
+
+// Converting string into an array ?
+// A String can be converted to an array with the split() method.
+// 3 way to convert the string into the array...
+// 1. split on commas (",");
+// 2. split on space (" ");
+// 3. split on pipe (" !");
+
+
+// 1. split on commas (",");
+
+// var text = "a, b , c, d, e, f";
+// console.log('original string ', text);
+// console.log(text.split(','));
+
+
+// 2. split on space (" ");
+// var text = "a, b , c, d, e, f";
+// console.log('original string ', text);
+// console.log('split on space ', text.split(' '));
+
+// // 3. split on pipe(" !");
+// var text = "a, | , b , | , c, d, |,  e, f";
+// console.log('original string ', text);
+// console.log('split on space ', text.split('|'));
+
+
+// if you used space separator operator  like split(" ") it return string in word format.
+
+// var string = "swagat, vishal, sudarshann, ganesh, ram ";
+// console.log('Original string', string);
+// let newArray = string.split(" ");
+// console.log('newArray', newArray);
+
+
+
+
+
+
+
 
 
 
